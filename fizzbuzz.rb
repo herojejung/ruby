25bd97c403145a71c5ce5dc3f23b21868874d6c9
YUTA KATSUBE <herojejung@users.noreply.github.com>
2022-10-08 15:11:58 +0000

    [add]ruby-8

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,18 @@
-def fizz_buzz (number)
-  "fizz" if number/=3
+def fizz_buzz(number)
+ if number % 15 == 0
+   "fizz_buzz"
+  elsif number % 3 == 0
+    "fizz"
+  elsif number % 5 == 0
+    "BUZZ"
+  else 
+    number.to_s
+ end
 end
 
-puts "数字を入力してください"
+puts "数字を入力してください。"
 
 input = gets.to_i
 
-puts "結果は。。。"
+puts "結果は..."
 puts fizz_buzz(input)
