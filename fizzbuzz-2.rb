6b6bfd3404503af5d9a71062caa67d3aa5d09615
YUTA KATSUBE <herojejung@users.noreply.github.com>
2022-10-09 16:08:18 +0000

    [finish]fizzbuzz-2

---
+++ fizzbuzz-2.rb
@@ -0,0 +1,18 @@
+def fizz_buzz (number)
+  if number % 15 == 0
+    puts "fizz_buzz"
+  elsif number % 3 == 0
+    puts "fizz"
+  elsif number % 5 == 0
+    puts "buzz"
+else
+   number.to_s
+end
+end
+
+puts "数字を入力してください"
+
+input = gets.to_i
+
+puts "結果は、、、"
+puts fizz_buzz(input)
